0a96f47f5922c3f8404ec11fb912b4e2bb22d7f8
YQY <545067444@qq.com>
2018-01-22 18:26:32 +0800

    小修改

--- JS/search.js
+++ JS/search.js
@@ -65,7 +65,7 @@ class Search{
             <li class='song-item'>
             <i class='icon icon-music'><img src="img/Music.png" alt=""></i>
                 <h6 class='song-name ellipsis'>${song.songname}</h6>
-                <p class='song-artist ellipsis'>${song.singer.map(s => s.name).join(" ")}</p>
+                <p class='song-artist ellipsis'>${song.singer.map(s => s.name).join(" ")}</p> 
             </li>`).join(" ")
 
         this.$songs.insertAdjacentHTML('beforeEnd',html)
